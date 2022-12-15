752af1048fe9d09428e73cccb06284b7d28e9eb8
Tony-Hsu <tony.hsu@jyintgroup.com>
2022-12-15 10:40:55 +0800

    ADD JPG

--- src/test/java/com/tony/tdd/tdddemo/TddDemoApplicationTests.java
+++ src/test/java/com/tony/tdd/tdddemo/TddDemoApplicationTests.java
@@ -16,17 +16,17 @@ class TddDemoApplicationTests {
     //假設有個空的購物籃 取得全部 預設是空的0.0
     @Test
     public void totalOfEmptyBasket() {
-        //假想 ShoppinBasket class 此时才创建
+        //假想 ShoppinBasket class 此時才創建
         ShoppinBasket basket = new ShoppinBasket(new ArrayList<>());
-        //假想 .getTotal()方法 返回 0.0 此时才创建 转跳 ShoppinBasket class
-        //使用assertEquals()断言返回直是否等于0.0
+        //假想 .getTotal()方法 返回 0.0 此時才創建 轉跳 ShoppinBasket class
+        //使用assertEquals()斷言返回直是否等於0.0
         assertEquals(0.0,basket.getTotal(),0.0);
     }
 
     @Test
-    //test 2 假设购物栏里有一个商品 商品100元*1
+    //test 2 假設購物欄里有一個商品 商品100元*1
     public void totalOfSingleItem(){
-        //因为需求需要List Item
+        //因為需求需要List Item
         ShoppinBasket basket = new ShoppinBasket(Arrays.asList(new Item(100.0,1)));
         assertEquals(100.0,basket.getTotal(),0.0);
 
@@ -34,7 +34,7 @@ class TddDemoApplicationTests {
 
 
     @Test
-    //test 3假设购物栏里有两个商品 商品100元*1  商品200元*1
+    //test 3假設購物欄里有兩個商品 商品100元*1  商品200元*1
     public void totalOfTwoItem(){
         ShoppinBasket basket = new ShoppinBasket(Arrays.asList(new Item(100.0 ,1),
                 new Item(200,1)));
@@ -43,14 +43,14 @@ class TddDemoApplicationTests {
     }
 
     @Test
-    //test 4假设购物栏里有两个商品 商品100元*2
+    //test 4假設購物欄里有兩個商品 商品100元*2
     public void totalOfItemWithQuantityTwo(){
         ShoppinBasket basket = buildBasketWithItems(new Item(100,2));
         assertEquals(200,basket.getTotal(),0.0);
 
     }
 
-//    优化
+//    優化
     private ShoppinBasket buildBasketWithItems(Item... items){
         return new ShoppinBasket(Arrays.asList(items));
     }
